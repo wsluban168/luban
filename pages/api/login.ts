dc02085733fde26b816d31dff98c6e0fdfac6709
wsluban168 <1353116457@qq.com>
2026-02-08 10:01:59 +0800

    更新 login.ts

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,1 +1,11 @@
-6
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { pwd } = JSON.parse(req.body || '{}')
+
+  if (pwd === process.env.ADMIN_PASSWORD) {
+    res.status(200).end()
+  } else {
+    res.status(401).end()
+  }
+}
