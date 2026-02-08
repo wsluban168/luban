022ebde3b7e1634cba0fdb7220db46b180dc4e76
wsluban168 <1353116457@qq.com>
2026-02-08 11:02:52 +0800

    更新 login.ts

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,6 +1,11 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    res.status(405).end('Method Not Allowed')
+    return
+  }
+
   const { pwd } = JSON.parse(req.body || '{}')
 
   if (pwd === process.env.ADMIN_PASSWORD) {
